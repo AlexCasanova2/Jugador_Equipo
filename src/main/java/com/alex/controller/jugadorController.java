faf656af2a43cf073e23af7f61567ec3418dfa63
Alex <casanovaalex11@gmail.com>
2016-10-28 16:31:58 +0200

    jugador controller (1)

--- src/main/java/com/alex/controller/jugadorController.java
+++ src/main/java/com/alex/controller/jugadorController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/jugadores")
 public class jugadorController{
+
     @Autowired
     private JugadorRepository jugadorRepository;
 
@@ -27,9 +28,14 @@ public class jugadorController{
         return jugadorRepository.findByNombre("Alex");
     }
 
-    @RequestMapping(value = "/byAsitencias{num}",
-    method = RequestMethod.GET)
-    public List<Jugador> findByAsistenciasGreater(@PathVariable Integer num){
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Jugador findById(@PathVariable Long id){
+        Jugador jugador = jugadorRepository.findOne(id);
+        return jugador;
+    }
+
+    @RequestMapping(value = "/byAsitencias{num}", method = RequestMethod.GET)
+    public List<Jugador> findByAsistenciasGreaterThan(@PathVariable Integer num){
         return jugadorRepository.findByAsistenciasGreaterThan(num);
     }
 
